114797d3c4fcc0b4a484e16ff1fb10bd72880a46
Miguel Ruiz <mig.ruiz@gmail.com>
2018-07-19 22:29:42 +0100

    condition

--- travis.sh
+++ travis.sh
@@ -1,4 +1,11 @@
-docker pull migruiz/$PI_APP-$PI_TYPE || true
-docker build -f Dockerfile_$PI_TYPE --cache-from migruiz/$PI_APP-$PI_TYPE -t migruiz/$PI_APP-$PI_TYPE . || travis_terminate 1
+#!/bin/bash  
+HUBNAME=""
+if [[ -z "$TRAVIS_TAG" ]]; then
+   HUBNAME=migruiz/$PI_APP-$PI_TYPE;
+else
+	HUBNAME=migruiz/$PI_APP-$PI_TYPE:$TRAVIS_TAG;
+fi
+docker pull $HUBNAME || true
+docker build -f Dockerfile_$PI_TYPE --cache-from $HUBNAME  -t $HUBNAME  . || travis_terminate 1
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin || travis_terminate 1
-docker push migruiz/$PI_APP-$PI_TYPE || travis_terminate 1
+docker push $HUBNAME  || travis_terminate 1
